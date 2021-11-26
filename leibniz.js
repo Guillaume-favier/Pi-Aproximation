7b71d39b6b3508dbf23ae6c4d30c1e71929e425f
Guillaume Favier <guillaume.favier.dev@gmail.com>
2021-11-26 21:28:36 +0100

    first commit

---
+++ leibniz.js
@@ -0,0 +1,8 @@
+﻿let n = 0
+let i = 1
+while(true){
+    if (i%2==1) n+=(1)/((i*2)-1)
+    else n-=1/((i*2)-1)
+    i++
+    if (i%10000000==0) process.stdout.write(" "+n*4+"\r")
+}
---
+++ random.js
@@ -0,0 +1,10 @@
+﻿it = 1000000000000000
+let i = 0
+let ratio = 0
+while (true){
+    let d = Math.sqrt((Math.random()*100-50)**2+(Math.random()*100-50)**2)
+    if (d<=50) ratio++
+    i++
+    if (i%8000000==0) process.stdout.write(" "+4*(ratio/i)+", "+i+",  "+ratio+"\r")
+}
+
